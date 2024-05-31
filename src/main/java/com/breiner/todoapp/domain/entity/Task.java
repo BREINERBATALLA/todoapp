8b7ac6d61df1c81dc30cc1b3360f39f5d5d0a402
Breiner <breiner.batalla@correounivalle.edu.co>
2024-05-30 20:46:12 -0500

    [ADD] task entity is added

---
+++ src/main/java/com/breiner/todoapp/domain/entity/Task.java
@@ -0,0 +1,28 @@
+package com.breiner.todoapp.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "task")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description", nullable = true, length = 500)
+    private String description;
+
+}
